b906e5edae85c86277f37351e0be00263ba8118c
Jana Šimíčková <jana.oravska@gmail.com>
2024-10-07 18:05:00 +0200

    Udelane priklady

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.body.innerHTML += "<p>Mesicni prijem divadla je " + prijem + "</p>"
 const prijemStudenti = ((174 * 0.4 * (0.65 * vstupne)) + (174 * 0.6 * vstupne)) * 15
 document.body.innerHTML +=  "<p>" + "Mesicni prijem divadla, kdyz tvori 40 % navstevniku studenti, je " + prijemStudenti + "</p>"
 
-
+542.88 + 1252.8
 
 document.body.innerHTML += "<p>priklad 2</p>"
 
@@ -17,3 +17,18 @@ document.body.innerHTML += "<p>priklad 2</p>"
 //Zamyslete se nad tím, zda vámi vytvořený výraz generuje všechna čísla skutečně se stejnou pravděpodobností. Vemte v úvahu, že funkce Math.random generuje náhodná čísla mezi 0 (včetně) a 1 (vyjma). Je tedy malinká pravěpodobnost, že občas padne přesně číslo 0. Naopak číslo 1 padnout nemůže.
 
 document.body.innerHTML += Math.ceil(Math.random() * 6)
+
+//Mějme v proměnné x uloženo nějaké desetinné číslo. Pomocí funkce Math.round jej můžeme zaokrouhlit na celá čísla takto.
+
+// Math.round(x);
+// Co kdybychom však chtěli zaokrouhlit na desetiny, setiny nebo třeba celé stovky? Napište program, který pomocí funkce Math.round
+
+// zaokrouhlí číslo x s přesností na desetiny,
+// zaokrouhlí číslo x s přesností na setiny,
+// zaokrouhlí číslo x s přesností na celé stovky.
+
+const x = 110.267
+document.body.innerHTML += "<p>" + Math.round(x / 100) *100 + "</p>"
+
+
+document.body.innerHTML += '1' + 2 + 3 + 4;
